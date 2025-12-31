7124ba4dd6be5e1e12d9d0afab8e87353a20723c
ExploitEngineer <revilwhoami0098@gmail.com>
2026-01-01 00:42:42 +0500

    chore: add Neue Montreal local fonts and setup next/font/local

---
+++ src/app/landing/_components/technology-section.tsx
@@ -0,0 +1,5 @@
+export function TechnologySection() {
+  return (
+    <section className="mx-auto mt-30 flex max-w-350 flex-col items-center gap-[4.38rem]"></section>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import neueMontreal from "../app/fonts/neue-montreal";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased ${neueMontreal.className}`}
       >
         {children}
       </body>
